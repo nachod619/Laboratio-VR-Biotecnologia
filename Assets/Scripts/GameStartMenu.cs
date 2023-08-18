8db369e93a9aa2d85e8f38c5af6071497db3de75
Ignacio Delgado <ignacio.delgadov@utem.cl>
2023-08-18 01:20:52 -0400

    Cambio de escena

--- Assets/Scripts/GameStartMenu.cs
+++ Assets/Scripts/GameStartMenu.cs
@@ -43,7 +43,7 @@ public class GameStartMenu : MonoBehaviour
     public void StartGame()
     {
         HideAll();
-        SceneTransitionManager.singleton.GoToSceneAsync(1);
+        SceneTransitionManager.singleton.GoToSceneAsync(2);
     }
 
     public void HideAll()
