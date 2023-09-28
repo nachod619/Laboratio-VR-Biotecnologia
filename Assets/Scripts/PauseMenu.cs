d4ddbe55caeea2450064ba8fbd57d1507be9096f
jajaime1 <jaime.ugazr@utem.cl>
2023-09-28 19:20:12 -0300

    Pause Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,78 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+
+    [Header("Game Pause Menu Buttons")]
+    public Button resumeButton;
+    public Button menuButton;
+    public Button quitButton;
+
+    public static bool GameIsPaused = false;
+
+    public List<Button> returnButtons;
+
+    public GameObject pauseMenuUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResumeGame();
+
+        resumeButton.onClick.AddListener(ResumeGame);
+        menuButton.onClick.AddListener(LoadMenu);
+        quitButton.onClick.AddListener(QuitGame);
+
+        foreach (var item in returnButtons)
+        {
+            item.onClick.AddListener(ResumeGame);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                ResumeGame();
+            }else
+            {
+                PauseGame();
+            }
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ResumeGame()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;  // descongela el juego de la pausa
+        GameIsPaused = false;
+    }
+
+    public void PauseGame()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;  // congela el juego en pausa 
+        GameIsPaused = true;
+    }
+
+    public void LoadMenu()
+    {
+        //SceneManager.LoadScene("Start Scene");
+        SceneTransitionManager.singleton.GoToSceneAsync(0);
+    }
+
+
+}
