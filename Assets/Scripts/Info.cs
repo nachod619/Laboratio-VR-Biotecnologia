e4fd322925970eb176b0f2227e03b83105c715fd
jajaime1 <jaime.ugazr@utem.cl>
2023-11-13 16:45:53 -0300

    Revert "etiquetas/info v1.1"
    This reverts commit 3a5371f2d9e5ce73318d406c3cd217e6aa4f78e8.

--- Assets/Scripts/Info.cs
+++ Assets/Scripts/Info.cs
@@ -126,51 +126,6 @@ public class Info : MonoBehaviour
         Popup.Show("Retículo endoplasmático rugoso");
     }
 
-    //info aparato de golgi
-
-    public void agButton1()
-    {
-        Popup.Show("Vesícula de ingreso o transición \n  Procedente del Retículo endoplasmático");
-    }
-
-    public void agButton2()
-    {
-        Popup.Show("Vesícula de salida");
-    }
-
-    public void agButton3()
-    {
-        Popup.Show("Glucolípidos");
-    }
-
-    public void agButton4()
-    {
-        Popup.Show("Vesícula en formación");
-    }
-
-    public void agButton5()
-    {
-        Popup.Show("Lisosoma");
-    }
-
-    public void agButton6()
-    {
-        Popup.Show("Espacio intercisternal");
-    }
-
-    public void agButton7()
-    {
-        Popup.Show("Glucoproteínas");
-    }
-
-
-    //info cilios-flagelos
-
-    public void mtButton1()
-    {
-        Popup.Show("Son apéndices locomotores que se prolongan desde la membrana de algunas células. Los cilios se encuentran solo en eucariotas; y, flagelos en procariotas y eucariotas; sin embargo las algas rojas, hongos, plantas con flor, nemátodos, no presentan ni cilios ni flagelos. En eucariotas, ambos presentan la misma estructura interna que está formada por el cuerpo basal y axonema. Los cillos realizan movimientos pendulares (circulares) y los flagelos ondulatorios, que les permiten remover, a modo de remos, el líquido que rodea la célula provocando el desplazamiento de ésta. Los flagelos se caracterizan por ser largos (100 - 200 micrómetros de longitud y 0,4 micrómetros de diámetro) mientras que los cilios son cortos (10-25 micrómetros y 0,25 micrómetros de diámetro). Los cilios presentes en mayor número (algunos miles), y flagelos muy pocos. Los procariotas pueden presentar un único flagelo, un grupo de éstos en un polo o múltiples flagelos que cubren toda la superficie; y, en eucariotas se encuentran en uno o dos.", longText);
-    }
-
     // Start is called before the first frame update
     void Start()
     {
